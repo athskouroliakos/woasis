d3b5c7c2fc70da2c1cc1c70e63d0c0827ee8a473
skouroliakos <thanosmw2@gmail.com>
2024-02-12 17:22:38 +0200

    ignore supabasejs

--- src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabaseUrl = 'https://rhaepikxrzveplcjxaic.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoYWVwaWt4cnp2ZXBsY2p4YWljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEzNDcxMTIsImV4cCI6MjAwNjkyMzExMn0.nyEB2CirWb45eFa9HUFt4EzVzAEdXbHYajk7gJ4LAEk';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
